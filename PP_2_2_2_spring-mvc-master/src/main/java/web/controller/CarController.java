da85fd60445b02ea2c2350248bb88a743a5102ae
PanurgRable <112486624+PanurgRable@users.noreply.github.com>
2023-01-10 21:15:47 +0300

    ed
    ed

--- PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -2,22 +2,21 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAO;
+import web.services.ServiceCars;
 
 @Controller
 public class CarController {
 
     @Autowired
-    private CarDAO carDAO;
+    private ServiceCars serviceCars;
 
     @GetMapping(value ="cars")
     public String showCars(@RequestParam(value = "count", required = false,
             defaultValue = "0") int i, ModelMap model) {
-        model.addAttribute("key", carDAO.show(i));
+        model.addAttribute("key", serviceCars.show(i));
         return "cars";
     }
 }
